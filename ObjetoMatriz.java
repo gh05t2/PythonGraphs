43e67535f396c7d26e98f30befdabc7ef107f50c
gh05t2 <44060435+gh05t2@users.noreply.github.com>
2020-01-24 18:08:09 +0100

    Update and rename JavaGraphs.java to ObjetoMatriz.java

---
+++ ObjetoMatriz.java
@@ -0,0 +1,16 @@
+package Grafos;
+public class MatrizAdyacencia{
+  private int filas;
+  private int columnas;
+  private String matriz[][];
+  
+  
+  /**
+    Constructor de objeto Matriz adyacencia
+  */
+  public MatrizAdyacencia(int filas,int columnas){
+    this.filas=filas;
+    this.columnas=columnas;
+    matriz=new String[this
+  }
+}
